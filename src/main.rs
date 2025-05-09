458e4a28cce9fa67cb1bde22ac62525d3a2f352d
ryokino <ryo.kinoshita212@gmail.com>
2025-05-09 20:19:24 +0900

    Running a web server which returns index.html using actix-web and actix-files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_files::Files;
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
+    HttpServer::new(|| App::new().service(Files::new("/", "./static").index_file("index.html")))
         .bind("127.0.0.1:8080")
         .expect("8080 port is already in use")
         .run()
         .await
-}
-
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World!")
 }
